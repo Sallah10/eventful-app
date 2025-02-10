d6c7d4b5d0177f030f516c26298a0acecca36997
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-02-10 15:44:06 +0100

    Add Buckets component (done) and integrate Axios and Vue Router dependencies

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Nav from './components/nav.vue';
 import Footer from './components/footer.vue';
 import Home from './components/home.vue';
+import Buckets from './components/buckets.vue';
 </script>
 
 <template>
   <div class="h-[100vh]">
     <Nav/>
     <Home/>
+    <Buckets/>
     <Footer/>
   </div>
 </template>
--- src/components/home.vue
+++ src/components/home.vue
@@ -2,26 +2,26 @@
 </script>
 
 <template>
-    <section class="text-black flex flex-col gap-6 px-10 mx-auto py-6">
-        <h1 class="font-bold text-[30px]">Welcome back,</h1>
-        <h2 class="font-regulartext-[20px]">
+    <section class="section">
+        <h1 class="textH1">Welcome back,</h1>
+        <h2 class="textH2">
             Hi, my name is Eventful Moments, I am a bucket… no, not the bucket of water but I store awesome moments you will like to have in coming years.
         </h2>
-        <form action="" class="flex flex-col gap-10">
+        <form action="" class="form">
             <div class="gap-2 flex flex-col">
-                <h3 class="font-regular text-[16px]">Email</h3>
+                <h3 class="textH3">Email</h3>
                 <input 
                 type="email"  
                 class="rounded-2xl w-full border-[#707070] border-2 px-6 py-4"/>
             </div>
             <div class="gap-2 flex flex-col">
-                <h3 class="font-regular text-[16px]">Password</h3>
+                <h3 class="textH3">Password</h3>
                 <input 
                 type="password"
                 class="rounded-2xl w-full border-[#707070] border-2 px-6 py-4"
                 />
             </div>
-            <button class="bg-[#5271FF] text-white rounded-2xl w-[235px] h-[64px] self-center">Login</button>
+            <button class="button">Login</button>
         </form>
     </section>
 </template>
