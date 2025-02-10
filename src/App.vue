acd7f34fc1ebc29f53f6edf780aed9014f01230c
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-02-10 16:45:56 +0100

    Add Register component and integrate it into App.vue; update nav styles

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,17 @@ import Home from './components/home.vue';
 import Buckets from './components/buckets.vue';
 import BucketInfo from './components/bucketInfo.vue';
 import AddItem from './components/addItem.vue';
+import Register from './components/register.vue';
 </script>
 
 <template>
-  <div class="h-[100vh]">
+  <div class="h-[100vh]  md:w-[100%]">
     <Nav/>
     <Home/>
     <Buckets/>
     <BucketInfo/>
     <AddItem/>
+    <Register/>
     <Footer/>
   </div>
 </template>
--- src/components/register.vue
+++ src/components/register.vue
@@ -1,3 +1,30 @@
 <template>
-    register
+    <section class="section">
+        <h1 class="textH1">Create an account,</h1>
+        <h2 class="textH2">
+            Hi, my name is Eventful Moments, I am a bucket… no, not the bucket of water but I store awesome moments you will like to have in coming years.
+        </h2>
+        <form action="" class="form">
+            <div class="gap-2 flex flex-col">
+                <h3 class="textH3">Fullname</h3>
+                <input 
+                type="text"  
+                class="rounded-2xl w-full border-[#707070] border-2 px-6 py-4"/>
+            </div>
+            <div class="gap-2 flex flex-col">
+                <h3 class="textH3">Email</h3>
+                <input 
+                type="email"  
+                class="rounded-2xl w-full border-[#707070] border-2 px-6 py-4"/>
+            </div>
+            <div class="gap-2 flex flex-col">
+                <h3 class="textH3">Password</h3>
+                <input 
+                type="password"
+                class="rounded-2xl w-full border-[#707070] border-2 px-6 py-4"
+                />
+            </div>
+            <button class="button">Create</button>
+        </form>
+    </section>
 </template>
