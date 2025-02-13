e065e6545342554a911274472d49a6e63c7d1261
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-02-13 16:27:11 +0100

    Fix import paths in App.vue to match component file naming conventions and adjust spacing in nav.vue

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -11,7 +11,7 @@ const isActiveLink = (routePath) => {
 <template>
     <nav class="bg-[#008289] flex justify-between text-[#ffffff] px-4 py-4 md:px-6 md:py-6">
         <RouterLink to="/buckets" class="italic text-xl">Eventful Moments.</RouterLink>
-        <ul class="flex gap-4 text-lg">
+        <ul class="flex gap-2 md:gap-4  text-lg">
             <li><RouterLink 
                 to="/"
                 :class="[
