0fb65e1794b7f2082ffaafec532dc298eb5a45a3
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-02-13 12:42:31 +0100

    built for production

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <script setup>
 import Nav from './components/Nav.vue';
 import Footer from './components/Footer.vue';
-// import Home from './components/Home.vue';
-// import Buckets from './components/Buckets.vue';
-// import BucketInfo from './components/BucketInfo.vue';
-// import AddItem from './components/AddItem.vue';
-// import Register from './components/Register.vue';
 import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="h-[100vh] ">
     <Nav/>
-    <!-- <Home/>
-    <Buckets/>
-    <BucketInfo/>
-    <AddItem/>
-    <Register/> -->
     <RouterView/>
     <Footer/>
   </div>
 
 </template>
 
+// import Home from './components/Home.vue';
+// import Buckets from './components/Buckets.vue';
+// import BucketInfo from './components/BucketInfo.vue';
+// import AddItem from './components/AddItem.vue';
+// import Register from './components/Register.vue';
